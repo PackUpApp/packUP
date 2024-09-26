30e5118a22039956a6ea58a70385d1262fe628ff
avery b <averwhy@gmail.com>
2024-09-26 19:39:35 -0400

    yet another test commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Time to packUP!</ThemedText>
+        <ThemedText type="title">packUP!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
