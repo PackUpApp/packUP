651851fa6f34c01f92e5767d75132a4f2c0ea96c
avery b <averwhy@gmail.com>
2024-11-04 15:58:06 -0500

    Fix small api issue

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -47,11 +47,8 @@ export default function TabTwoScreen() {
       </Collapsible>
       <Collapsible title="Appearance">
         <ThemedText>
-          Current theme is:
-          <ThemedText type="defaultSemiBold">
-            {" "}
-            {colorScheme} {"\n"}
-          </ThemedText>
+          Current theme is: 
+          <ThemedText type="defaultSemiBold"> {colorScheme} {"\n"}</ThemedText>
           You can change this via your phone's settings.
         </ThemedText>
       </Collapsible>
