5df56ca2b02449c8c52190c42c8e3b412c84b26b
brennandio <brennandiokh@gmail.com>
2024-11-07 17:09:00 -0500

    Start questionairre

---
+++ app/(tabs)/questionarre.tsx
@@ -0,0 +1,89 @@
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { apiBaseUrl } from "@/constants/Host";
+import { useQuery } from "@tanstack/react-query";
+import { Image, StyleSheet, TextInput, Text, TouchableOpacity } from "react-native";
+
+export default function HomeScreen() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["sync"],
+    async queryFn() {
+      const res = await fetch(`${apiBaseUrl}/user/b879bc2a-8817-47e6-ab12-4ad86785223e`, {
+        headers: new Headers({
+          Authorization: "Bearer 123",
+        }),
+      });
+
+      if (!res.ok) throw new Error(res.statusText);
+
+      const text = await res.json();
+      return text;
+    },
+  });
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">packUP!</ThemedText>
+      </ThemedView>
+
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">
+          {isPending ? "Loading…" : error ? "An error occurred" : `Welcome back ${data.fname} ${data.lname}!`}
+        </ThemedText>
+        <HelloWave />
+      </ThemedView>
+
+      <ThemedView style={styles.button}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Create New Trip</Text>
+        </TouchableOpacity>
+      </ThemedView>
+
+      {/* If you need a TextInput, use it here for user input */}
+      <ThemedView style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your trip name"
+        />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+
+  button: {
+    padding: 15,
+    backgroundColor: "#add8e6",
+    borderRadius: 30,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "black",
+  },
+});
