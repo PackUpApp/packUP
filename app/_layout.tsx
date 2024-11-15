d9ee333b95050bd1137612ca8dd28ab53c798543
avery <averwhy@gmail.com>
2024-11-14 19:59:18 -0500

    Add readme + eas config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout() {
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
- 
-
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
